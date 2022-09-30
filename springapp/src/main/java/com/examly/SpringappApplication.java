a33e3e392480ed8663a439bdef25a169d9c50186
mohammedsahebpatel <mdsahebpatel21@gmail.com>
2022-09-30 06:07:40 +0000

    initial commit

---
+++ springapp/src/main/java/com/examly/SpringappApplication.java
@@ -0,0 +1,13 @@
+package com.examly.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappApplication {
+
+    Run| Debug
+    public static void main(String[] args) {
+        SpringApplication.run(SpringappApplication.class,args);s
+    }
+}
